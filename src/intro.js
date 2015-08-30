121153bbed35e90e4082152b25d1f7b677e62dce
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-08-30 19:28:03 -0400

    Adding a condition so node uses the module (it's roughly 2x as fast right now)

--- src/intro.js
+++ src/intro.js
@@ -1,2 +1,3 @@
 (function (global) {
-const Promise = global.Promise || require("es6-promise").Promise;
+const server = typeof process !== "undefined" && typeof process.nextTick === "function";
+const Promise = !server && global.Promise ? global.Promise : require("es6-promise").Promise;
