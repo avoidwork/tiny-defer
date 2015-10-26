690f16e56d3a52b9aa32a972c0dff43162190146
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-10-26 07:06:48 -0400

    Using global `Promise` if available (native is best)

--- src/intro.js
+++ src/intro.js
@@ -1,3 +1,2 @@
 (function (global) {
-const server = typeof process !== "undefined" && typeof process.nextTick === "function";
-const Promise = !server && global.Promise ? global.Promise : require("es6-promise").Promise;
+const Promise = global.Promise || require("es6-promise").Promise;
